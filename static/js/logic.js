c926d2fe11476363a4a23958c8844b9bbf3730c9
Lolita Dias <lolita.dias@gmail.com>
2020-01-25 15:29:31 -0500

    uploading files

---
+++ static/js/logic.js
@@ -0,0 +1,109 @@
+// Store our API endpoint inside queryUrl - Past 7 days
+// var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson";
+// Store our API endpoint inside queryUrl - Past 30 days
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+// Initialize & Create Two Separate LayerGroups: earthquakes & tectonicPlates
+var earthquakes = new L.LayerGroup();
+
+// Define Variables for Tile Layers
+var satelliteMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: API_KEY
+});
+
+var grayscaleMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.light",
+    accessToken: API_KEY
+});
+
+var outdoorsMap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.outdoors",
+    accessToken: API_KEY
+});
+
+// Define baseMaps Object to Hold Base Layers
+var baseMaps = {
+    "Satellite": satelliteMap,
+    "Grayscale": grayscaleMap,
+    "Outdoors": outdoorsMap
+};
+
+// Create Overlay Object to Hold Overlay Layers
+var overlayMaps = {
+    "Earthquakes": earthquakes,
+};
+
+// Create Map, Passing In satelliteMap & earthquakes as Default Layers to Display on Load
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 3,
+    layers: [satelliteMap, earthquakes]
+});
+
+// Create a Layer Control + Pass in baseMaps and overlayMaps + Add the Layer Control to the Map
+L.control.layers(baseMaps, overlayMaps).addTo(myMap);
+
+// Retrieve earthquakesURL (USGS Earthquakes GeoJSON Data) with D3
+d3.json(queryUrl, function(earthquakeData) {
+    // Function to Determine Size of Marker Based on the Magnitude of the Earthquake
+    function markerSize(magnitude) {
+        if (magnitude === 0) {
+          return 1;
+        }
+        return magnitude * 3;
+    }
+    // Function to Determine Style of Marker Based on the Magnitude of the Earthquake
+    function styleInfo(feature) {
+        return {
+          opacity: 1,
+          fillOpacity: 1,
+          fillColor: defineColor(feature.properties.mag),
+          color: "black",
+          radius: markerSize(feature.properties.mag),
+          stroke: true,
+          weight: 0.5
+        };
+    }
+
+    // Function to Determine Color of Marker Based on the Magnitude of the Earthquake
+    function defineColor(magnitude) {
+        switch (true) {
+        case magnitude > 5:
+            return "sienna";
+        case magnitude > 4:
+            return "orange";
+        case magnitude > 3:
+            return "salmon";
+        case magnitude > 2:
+            return "yellow";
+        case magnitude > 1:
+            return "green";
+        default:
+            return "magic mint";
+        }
+    }
+    // Create a GeoJSON Layer Containing the Features Array on the earthquakeData Object
+    L.geoJSON(earthquakeData, {
+        pointToLayer: function(feature, latlng) {
+            return L.circleMarker(latlng);
+        },
+        style: styleInfo,
+        // Function to Run Once For Each feature in the features Array
+        // Give Each feature a Popup Describing the Place & Time of the Earthquake
+        onEachFeature: function(feature, layer) {
+            layer.bindPopup("<h4>Location: " + feature.properties.place + 
+            "</h4><hr><p>Date & Time: " + new Date(feature.properties.time) + 
+            "</p><hr><p>Magnitude: " + feature.properties.mag + "</p>");
+        }
+    // Add earthquakeData to earthquakes LayerGroups 
+    }).addTo(earthquakes);
+    // Add earthquakes Layer to the Map
+    earthquakes.addTo(myMap);
+});
